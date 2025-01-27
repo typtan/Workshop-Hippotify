32282b8478fc9bd141cd1add01ba430c31ad534c
typtan <thanyaporn.chankajorn@gmail.com>
2025-01-27 16:34:14 +0700

    connect database

---
+++ server/src/config/database.js
@@ -0,0 +1,14 @@
+import mysql from "mysql2";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+})
+
+export default connection;
---
+++ server/src/utils/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import connection from "../config/database.js";
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+connection.connect((err) => {
+    if(err){
+        console.log(err);
+    }else{
+        console.log('Database is connected');
+    }
+})
+
+app.listen(port,() => {
+    console.log(`App listening on port ${port}`);
+})
